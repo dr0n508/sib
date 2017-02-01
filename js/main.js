51f3766b45cd45331aaf8883487ffedae83a5ee9
kornienko.andriy <kornienko.andriy@gmail.com>
2017-02-01 08:39:11 +0200

    some part future slide

--- js/main.js
+++ js/main.js
@@ -12,19 +12,30 @@ $(document).ready(function(){
     });
 
 
+
+    /***slider***/
+
+
     var numberSlides = $('.carousel ul li').length;
     var widthSlide = $('.carousel ul li').width();
     console.log(widthSlide);
     console.log(widthSlide*numberSlides);
     $('.carousel ul li.active').width(widthSlide);
-    $('.carousel ul li.not-active').width(600);
+    $('.carousel ul li.not-active').width(150);
     $('.carousel ul').width(numberSlides*widthSlide);
 
-    var left = 600;
+    var left = 150;
     $( "#i-see-video" ).click(function() {
         $(".carousel ul").css('marginLeft', -left+'px');
-        $(".carousel ul li.active").removeClass("active").addClass('not-active').width(600).next('.carousel ul li').removeClass("not-active").addClass('active').width(widthSlide);
-        left = left + 605;
+        $(".carousel ul li.active").removeClass("active").addClass('not-active').width(150).next('.carousel ul li').removeClass("not-active").addClass('active').width(widthSlide);
+        left = left + 155;
+    });
+
+    /****pause video****/
+    $(document).on('click', '#close_vid', function() {
+        jQuery("iframe").each(function() {
+            jQuery(this)[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
+        });
     });
 
 
